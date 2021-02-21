9629137b9c7fd46fb2ca2c030d4eb0676cdf9701
YenHungLai <jacoblai1029@gmail.com>
2021-02-20 17:31:22 -0700

    Update form

--- pages/order/[id]/failure/index.js
+++ pages/order/[id]/failure/index.js
@@ -1,37 +1,52 @@
-import { useState } from 'react';
-import styles from './index.module.css';
+import { useState } from "react";
+import moment from "moment";
+import styles from "./index.module.css";
+
+const REASONS = ["Nobody was home", "You didn't sign", "Door was locked"];
 
 const Failure = () => {
-	const [form, setForm] = useState({});
+    const [form, setForm] = useState({});
 
-	const onChange = (e) => {
-		setForm({ ...form, [e.target.name]: e.target.value });
-	};
+    const onChange = (e) => {
+        setForm({ ...form, [e.target.name]: e.target.value });
+    };
 
-	const onClick = (e) => {
-		e.preventDefault();
+    const onClick = (e) => {
+        e.preventDefault();
 
-		console.log(form);
-	};
+        console.log(form);
+    };
 
-	return (
-		<div className={styles.root}>
-			<h1>Failure!</h1>
-			<form className={styles.form}>
-				<div className={styles.recipient}>
-					<label htmlFor='recipient'>Reason for failure</label>
-					<input type='text' name='reason' onChange={onChange} />
-				</div>
-				<div>
-					<label htmlFor='failure-time'>Failure time</label>
-					<input type='datetime-local' name='failure-time' onChange={onChange} />
-				</div>
-				<button onClick={onClick} className={styles.submit} type='submit'>
-					Submit
-				</button>
-			</form>
-		</div>
-	);
+    return (
+        <div className={styles.root}>
+            <h1>Failure!</h1>
+            <form className={styles.form}>
+                <div className={styles.recipient}>
+                    <label htmlFor="recipient">Reason for failure</label>
+                    <select name="reason" onChange={onChange}>
+                        <option default>Please select a reason</option>
+                        {REASONS.map((reason, idx) => (
+                            <option key={idx} value={reason}>
+                                {reason}
+                            </option>
+                        ))}
+                    </select>
+                </div>
+                <div>
+                    <label htmlFor="failure-time">Failure time</label>
+                    <input
+                        readOnly
+                        type="text"
+                        name="failure-time"
+                        defaultValue={moment().format("YYYY/MM/DD HH:mm:ss")}
+                    />
+                </div>
+                <button onClick={onClick} className={styles.submit} type="submit">
+                    Submit
+                </button>
+            </form>
+        </div>
+    );
 };
 
 export default Failure;
--- pages/order/[id]/success/index.js
+++ pages/order/[id]/success/index.js
@@ -1,37 +1,52 @@
-import { useState } from 'react';
-import styles from './index.module.css';
+import { useState } from "react";
+import moment from "moment";
+import styles from "./index.module.css";
+
+const RECIPIENT = ["Person_A", "Person_B", "Person_C"];
 
 const Success = () => {
-	const [form, setForm] = useState({});
+    const [form, setForm] = useState({});
 
-	const onChange = (e) => {
-		setForm({ ...form, [e.target.name]: e.target.value });
-	};
+    const onChange = (e) => {
+        setForm({ ...form, [e.target.name]: e.target.value });
+    };
 
-	const onClick = (e) => {
-		e.preventDefault();
+    const onClick = (e) => {
+        e.preventDefault();
 
-		console.log(form);
-	};
+        console.log(form);
+    };
 
-	return (
-		<div className={styles.root}>
-			<h1>Success!</h1>
-			<form className={styles.form}>
-				<div className={styles.recipient}>
-					<label htmlFor='recipient'>Recipient</label>
-					<input type='text' name='recipient' onChange={onChange} />
-				</div>
-				<div>
-					<label htmlFor='sign-time'>Signed at</label>
-					<input type='datetime-local' name='sign-time' onChange={onChange} />
-				</div>
-				<button onClick={onClick} className={styles.submit} type='submit'>
-					Submit
-				</button>
-			</form>
-		</div>
-	);
+    return (
+        <div className={styles.root}>
+            <h1>Success!</h1>
+            <form className={styles.form}>
+                <div className={styles.recipient}>
+                    <label htmlFor="recipient">Recipient</label>
+                    <select name="recipient" onChange={onChange}>
+                        <option default>Select a recipient</option>
+                        {RECIPIENT.map((recipient, idx) => (
+                            <option key={idx} value={recipient}>
+                                {recipient}
+                            </option>
+                        ))}
+                    </select>
+                </div>
+                <div>
+                    <label htmlFor="sign-time">Signed at</label>
+                    <input
+                        readOnly
+                        type="text"
+                        name="sign-time"
+                        defaultValue={moment().format("YYYY/MM/DD HH:mm:ss")}
+                    />
+                </div>
+                <button onClick={onClick} className={styles.submit} type="submit">
+                    Submit
+                </button>
+            </form>
+        </div>
+    );
 };
 
 export default Success;
