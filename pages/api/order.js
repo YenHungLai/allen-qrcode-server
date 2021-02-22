e02607bdfff24db881d1fa3bbbe2c80813f12f35
YenHungLai <jacoblai1029@gmail.com>
2021-02-21 17:59:32 -0700

    Update API

--- pages/api/order.js
+++ pages/api/order.js
@@ -1,23 +1,26 @@
 import oracledb from 'oracledb';
 
-export default (req, res) => {
+export default async (req, res) => {
 	console.log(req.body);
+	const { reason, failedAt } = req.body;
 
-  let connection;
-  try {
+	let connection;
+	try {
 		connection = await oracledb.getConnection({
 			user: 'JITIAN',
 			password: 'Logic168',
-			connectString: '192.168.137.1/ORCL',
+			connectString: '192.168.137.1/JITIAN',
 		});
 
-		const result = await connection.execute();
-		console.log(result.rows);
+		const result = await connection.execute(
+			`INSERT INTO TEST(REASON,REASON_DATE,HWB) VALUES (${reason}, ${failedAt},'')`
+		);
+		connection.commit();
 
 		res.json({ data: result.rows });
 	} catch (err) {
 		console.error(err);
-    res.json({ msg: err });
+		res.json({ msg: err });
 	} finally {
 		if (connection) {
 			try {
--- pages/order/[id]/failure/index.js
+++ pages/order/[id]/failure/index.js
@@ -1,57 +1,57 @@
-import { useState } from "react";
+import { useState } from 'react';
 import axios from 'axios';
-import moment from "moment";
-import styles from "./index.module.css";
+import moment from 'moment';
+import styles from './index.module.css';
 
-const REASONS = ["Nobody was home", "You didn't sign", "Door was locked"];
+const REASONS = ['Nobody was home', "You didn't sign", 'Door was locked'];
 
 const Failure = () => {
-    const [form, setForm] = useState({});
+	const [form, setForm] = useState({});
 
-    const onChange = (e) => {
-        setForm({ ...form, [e.target.name]: e.target.value });
-    };
+	const onChange = (e) => {
+		setForm({ ...form, [e.target.name]: e.target.value });
+	};
 
-    const onClick = (e) => {
-        e.preventDefault();
+	const onClick = async (e) => {
+		e.preventDefault();
 
-        console.log(form);
+		console.log(form);
 		await axios.put(`${window.location.origin}/api/order`, {
 			...form,
-			signedAt: new Date().toISOString(),
+			failedAt: new Date().toISOString(),
 		});
-    };
+	};
 
-    return (
-        <div className={styles.root}>
-            <h1>Failure!</h1>
-            <form className={styles.form}>
-                <div className={styles.recipient}>
-                    <label htmlFor="recipient">Reason for failure</label>
-                    <select name="reason" onChange={onChange}>
-                        <option default>Please select a reason</option>
-                        {REASONS.map((reason, idx) => (
-                            <option key={idx} value={reason}>
-                                {reason}
-                            </option>
-                        ))}
-                    </select>
-                </div>
-                <div>
-                    <label htmlFor="failure-time">Failure time</label>
-                    <input
-                        readOnly
-                        type="text"
-                        name="failure-time"
-                        defaultValue={moment().format("YYYY/MM/DD HH:mm:ss")}
-                    />
-                </div>
-                <button onClick={onClick} className={styles.submit} type="submit">
-                    Submit
-                </button>
-            </form>
-        </div>
-    );
+	return (
+		<div className={styles.root}>
+			<h1>Failure!</h1>
+			<form className={styles.form}>
+				<div className={styles.recipient}>
+					<label htmlFor='recipient'>Reason for failure</label>
+					<select name='reason' onChange={onChange}>
+						<option default>Please select a reason</option>
+						{REASONS.map((reason, idx) => (
+							<option key={idx} value={reason}>
+								{reason}
+							</option>
+						))}
+					</select>
+				</div>
+				<div>
+					<label htmlFor='failure-time'>Failure time</label>
+					<input
+						readOnly
+						type='text'
+						name='failure-time'
+						defaultValue={moment().format('YYYY/MM/DD HH:mm:ss')}
+					/>
+				</div>
+				<button onClick={onClick} className={styles.submit} type='submit'>
+					Submit
+				</button>
+			</form>
+		</div>
+	);
 };
 
 export default Failure;
